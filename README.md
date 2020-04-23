74e6b816b1d4190091603138503634ac8b015530
simicd <10134699+simicd@users.noreply.github.com>
2020-04-23 21:34:59 +0200

    Read requirements file in setup.py

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This repo stores a template for Python packages and includes
 - Simple folder structure with test folder
 - Files with dependencies (requirements.txt)
-- Installation instructions (setup.py) which can be run with `python setup.py sdist`
+- Installation instructions (setup.py) which generates a distributable Python package with `python setup.py bdist_wheel --universal` (might require `pip install wheel` beforehand)
 
 ## Documentation with Sphinx
 - Create module structure as .rst file (required when there are new .py modules): `sphinx-apidoc -o docs/source/ pytemplate pytemplate/tests/* --force --separate --no-headings`. Options:
--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 import setuptools
 
+# Get a long description from the README file
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
+# Get dependencies file and convert it to list of strings
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
 
 setuptools.setup(
     name="example-pkg-your-username",
@@ -14,8 +19,11 @@ setuptools.setup(
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: OS Independent"
     ],
+    python_requires='>=3.6',
+    install_requires=required
 )
